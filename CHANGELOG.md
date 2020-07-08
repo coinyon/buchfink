1c16c88f8136acca9dcc685d2e3d21d95cb11e81
Aleister Coinley <coinyon@uberhax0r.de>
2020-07-08 12:42:06 +0200

    Bump version and add CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+0.0.3
+-----
+
+* Balances will now also show BTC, ETH and ERC20 blockchain balances
+* Add `allowances` command to show the assets that can be sold tax-free
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requirements = list(set(
     if not requirement.lstrip().startswith('#')
 ))
 
-version = '0.0.1'
+version = '0.0.3'
 
 setup(
     name='buchfink',
