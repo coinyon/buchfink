a2f7748e05bfd750c29cf78de2e9314260347717
Aleister Coinley <coinyon@uberhax0r.de>
2021-02-08 21:14:21 +0100

    Collect actions as well

--- buchfink/report.py
+++ buchfink/report.py
@@ -18,18 +18,20 @@ def run_report(buchfink_db: BuchfinkDB, report_config: ReportConfig):
     end_ts = report_config.to_dt.timestamp()
     num_matched_accounts = 0
     all_trades = []
+    all_actions = []
 
     logger.info('Generating report "%s"...', name)
 
     for account in buchfink_db.get_all_accounts():
         num_matched_accounts += 1
         all_trades.extend(buchfink_db.get_local_trades_for_account(account.name))
+        all_actions.extend(buchfink_db.get_local_ledger_actions_for_account(account.name))
 
-    logger.info('Collected %d trades from %d exchange account(s)',
-            len(all_trades), num_matched_accounts)
+    logger.info('Collected %d trades / %d actions from %d exchange account(s)',
+            len(all_trades), len(all_actions), num_matched_accounts)
 
     accountant = buchfink_db.get_accountant()
-    result = accountant.process_history(start_ts, end_ts, all_trades, [], [], [], [], [])
+    result = accountant.process_history(start_ts, end_ts, all_trades, [], [], [], [], all_actions)
     accountant.csvexporter.create_files(buchfink_db.reports_directory / Path(name))
 
     with (buchfink_db.reports_directory / Path(name) / 'report.yaml').open('w') as report_file:
