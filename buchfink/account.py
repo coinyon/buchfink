ebba8b5cee69d007c8c4441ef3ea7642639b7614
Aleister Coinley <coinyon@uberhax0r.de>
2021-03-06 12:21:22 +0100

    Fix linter warning

--- buchfink/account.py
+++ buchfink/account.py
@@ -40,9 +40,9 @@ def accounts_from_config(config) -> List[Account]:
     return [account_from_config(acc) for acc in config['accounts']]
 
 
-def account_from_string(acc_def: str, db) -> Account:
+def account_from_string(acc_def: str, buchfink_db) -> Account:
     if acc_def.lower().endswith('.eth'):
-        eth_address = db.ethereum_manager.ens_lookup(acc_def)
+        eth_address = buchfink_db.ethereum_manager.ens_lookup(acc_def)
         if not eth_address:
             raise ValueError(f'Could not resolve ENS: {acc_def}')
         return Account(acc_def, 'ethereum', eth_address, {})
