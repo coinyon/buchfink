4b9d41c30cf41ec4f057b78fe92f52e9bfe8c04e
Aleister Coinley <coinyon@uberhax0r.de>
2021-07-25 17:56:13 +0200

    Add FOX airdrop and UMA KPI settle

--- buchfink/classification.py
+++ buchfink/classification.py
@@ -15,6 +15,7 @@ CLAIMED = '0x4ec90e965519d92681267467f775ada5bd214aa92c0dc93d90a5e880ce9ed026'
 CLAIMED_2 = '0xd8138f8a3f377c5259ca548e70e4c2de94f129f5a11036a15b69513cba2b426a'
 CLAIMED_3 = '0x6f9c9826be5976f3f82a3490c52a83328ce2ec7be9e62dcb39c26da5148d7c76'
 CLAIMED_4 = '0x04672052dcb6b5b19a9cc2ec1b8f447f1f5e47b5e24cfa5e4ffb640d63ca2be7'
+CLAIMED_5 = '0x528937b330082d892a98d4e428ab2dcca7844b51d227a1c0ae67f0b5261acbd9'
 TRANSFER = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
 REWARD_PAID = '0xe2403640ba68fed3a2f88b7557551d1993f84b99bb10ff833f0cf8db0c5e0486'
 MINTED = '0x9d228d69b5fdb8d273a2336f8fb8612d039631024ea9bf09c424a9503aa078f0'
@@ -41,25 +42,32 @@ ADDR_CREAM_REWARDS = (
 ADDR_BALANCER_REWARDS = '0x6d19b2bF3A36A61530909Ae65445a906D98A2Fa8'
 ADDR_POOL_AIRDROP = '0xBE1a33519F586A4c8AA37525163Df8d67997016f'
 ADDR_BADGER_TREE = '0x660802Fc641b154aBA66a62137e71f331B6d787A'
-ADDR_BADGER = '0x3472A5A71965499acd81997a54BBA8D852C6E53d'
 ADDR_MIR_REWARDS = '0x5d447Fc0F8965cED158BAB42414Af10139Edf0AF'
 ADDR_XTOKEN_AIRDROP = '0x11f10378fc56277eEdBc0c3309c457b0fd5c6dfd'
 ADDR_SWERVE_MINTER = '0x2c988c3974ad7e604e276ae0294a7228def67974'
 ADDR_FEI_GENESIS_GROUP = '0xBFfB152b9392e38CdDc275D818a3Db7FE364596b'
 ADDR_DODO_REWARDS = '0x0e504d3e053885a82bd1cb5c29cbaae5b3673be4'
-ADDR_DODO = '0x43Dfc4159D86F3A37A5A4B3D4580b888ad7d4DDd'
 ADDR_IMX_AIRDROP = '0x2011b5d4d5287cc9d3462b4e8af0e4daf29e3c1d'
 ADDR_ROOK_REWARDS = '0x2777b798fdfb906d42b89cf8f9de541db05dd6a1'
 ADDR_SUSHI_REWARDS = '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd'
-ADDR_SUSHI = '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2'
 ADDR_GITCOIN_AIRDROP = '0xde3e5a990bce7fc60a6f017e7c4a95fc4939299e'
 ADDR_BLACKPOOL_AIRDROP = '0x6b63564a8b3f145b3ef085bcc197c0ff64e9a140'
-ADDR_TORN = '0x77777feddddffc19ff86db637967013e6c6a116c'
 ADDR_TORN_VTORN = '0x3eFA30704D2b8BBAc821307230376556cF8CC39e'
-ADDR_HEX = '0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39'
 ADDR_XTK_VESTING = '0x2ac34f8327aceD80CFC04085972Ee06Be72A45bb'
+ADDR_FOX_AIRDROP = '0xd1Fa5AA6AD65eD6FEA863c2e7fB91e731DcD559F'
+ADDR_FOX_AIRDROP_2 = '0x91B9A78658273913bf3F5444Cb5F2592d1123eA7'
+ADDR_FOX_AIRDROP_3 = '0xf4BBE639CCEd35210dA2018b0A31f4E1449B2a8a'
+ADDR_FOX_AIRDROP_4 = '0x7BC08798465B8475Db9BCA781C2Fd6063A09320D'
+ADDR_UMA_TVL_OPT = '0x0Ee5Bb3dEAe8a44FbDeB269941f735793F8312Ef'
+
+ADDR_DODO = '0x43Dfc4159D86F3A37A5A4B3D4580b888ad7d4DDd'
+ADDR_SUSHI = '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2'
+ADDR_TORN = '0x77777feddddffc19ff86db637967013e6c6a116c'
+ADDR_HEX = '0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39'
 ADDR_COMPOUND_DAI = '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643'
 ADDR_DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+ADDR_BADGER = '0x3472A5A71965499acd81997a54BBA8D852C6E53d'
+ADDR_UMA = '0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828'
 
 
 def classify_tx(account: Account, tx_hash: str, txn: EthereumTransaction, receipt: dict) \
@@ -219,6 +227,29 @@ def classify_tx(account: Account, tx_hash: str, txn: EthereumTransaction, receip
             )]
 
         elif event['topics'][0] == CLAIMED_4:
+            logger.warning('Unknown Claimed event for tx: %s', tx_hash)
+
+        if event['topics'][0] == CLAIMED_5 and event['address'] in (
+                ADDR_FOX_AIRDROP.lower(),
+                ADDR_FOX_AIRDROP_2.lower(),
+                ADDR_FOX_AIRDROP_3.lower(),
+                ADDR_FOX_AIRDROP_4.lower()
+                ):
+            amount = hexstr_to_int(event['data'][2:][64:128])
+            actions += [LedgerAction(
+                identifier=None,
+                location='',
+                action_type=LedgerActionType.AIRDROP,
+                amount=FVal(amount) / FVal(1e18),
+                rate=None,
+                rate_asset=None,
+                timestamp=txn.timestamp,
+                asset=symbol_to_asset_or_token('_ceth_0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d'),
+                notes='FOX token airdrop',
+                link=tx_hash
+            )]
+
+        elif event['topics'][0] == CLAIMED_5:
             logger.warning('Unknown Claimed event for tx: %s', tx_hash)
 
         if event['topics'][0] == REWARD_PAID and event['address'] in ADDR_PIEDAO_INCENTIVES:
@@ -407,6 +438,25 @@ def classify_tx(account: Account, tx_hash: str, txn: EthereumTransaction, receip
                     timestamp=txn.timestamp,
                     asset=asset,
                     notes='DAI mint',
+                    link=tx_hash
+                )]
+
+        elif event['topics'][0] == TRANSFER and event['address'] == ADDR_UMA.lower():
+            # UMA TVL Option Settlement
+            asset = symbol_to_asset_or_token('_ceth_' + ADDR_UMA)
+            if hexstr_to_int(event['topics'][1]) == hexstr_to_int(ADDR_UMA_TVL_OPT) and \
+                    hexstr_to_int(event['topics'][2]) == hexstr_to_int(account.address):
+                amount = hexstr_to_int(event['data'])
+                actions += [LedgerAction(
+                    identifier=None,
+                    location='',
+                    action_type=LedgerActionType.INCOME,
+                    amount=FVal(amount) / FVal(1e18),
+                    rate=None,
+                    rate_asset=None,
+                    timestamp=txn.timestamp,
+                    asset=asset,
+                    notes='UMA TVL option settlement',
                     link=tx_hash
                 )]
 
