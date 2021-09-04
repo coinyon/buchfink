ff2f7cb6e05d9eedad13962123cb294b73c06eff
Aleister Coinley <coinyon@uberhax0r.de>
2021-09-04 10:02:47 +0200

    Rename test case to manual_price

--- tests/test_report.py
+++ tests/test_report.py
@@ -22,7 +22,7 @@ def test_bullrun_config():
     assert result['overview']['total_taxable_profit_loss'] == '15000'
 
 
-def test_unknown_price():
+def test_manual_price():
     buchfink_db = BuchfinkDB(os.path.join(os.path.dirname(__file__), 'scenarios', 'ledger_actions'))
 
     buchfink_db.apply_manual_prices()
@@ -37,11 +37,11 @@ def test_unknown_price():
     assert report.from_dt.year == 2020
     assert report.to_dt.year == 2021
 
-    acc = [a for a in buchfink_db.get_all_accounts() if a.name=='acc_unknown_price']
+    acc = [a for a in buchfink_db.get_all_accounts() if a.name=='acc_manual_price']
 
     result = run_report(buchfink_db, acc, report)
 
     # Even we made 30 USD, only 20 is taxable, because
-    # price was 0.01 at buy (see acc_unknown_price.yaml)
+    # price was 0.01 at buy (see acc_manual_price.yaml)
     # and 0.03 at sell (coingecko)
     assert float(result['overview']['total_taxable_profit_loss']) == 20.0
